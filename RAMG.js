a14e6feec59fbdec57526c1070896a5c9c207aa2
Xun_and_Moon <mr.karr294@gmail.com>
2025-10-16 16:23:45 +0800

    Added copyright to bottom of program

--- RAMG.js
+++ RAMG.js
@@ -169,3 +169,7 @@ const fullRandomGenerator = () => {
 // Generate the full comment
 console.log(fullRandomGenerator());
 
+/* RAMG.js Copyright (C) 2025  Xun and Moon
+    This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+    This is free software, and you are welcome to redistribute it
+    under certain conditions; type `show c' for details. */
